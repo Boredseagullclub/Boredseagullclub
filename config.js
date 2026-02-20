748ccc4b154029e6d50d1968291272f6c8adc3cf
Bored Seagull Club <134111642+Boredseagullclub@users.noreply.github.com>
2026-02-20 12:05:34 -0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    TOKENS: {
+        XRP: { type: "XRP", issuer: "rnqiA8vuNriU9pqD1ZDGFH8ajQBL25Wkno" },
+        FLR: { type: "EVM", contract: "0x495dafa49ed19f3bfc3ddeb7e048f20ff149778f" },
+        XDC: { type: "EVM", contract: "0xd38109f587bd0326cad60a18cf3c1ecd546809a6" },
+    },
+    DEFAULT_FEE: 0.025
+};
