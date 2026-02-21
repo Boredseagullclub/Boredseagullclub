349cd80ea3cbc82d75e454df4bc180b3874bf9b0
Bored Seagull Club <134111642+Boredseagullclub@users.noreply.github.com>
2026-02-21 11:03:12 -0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,7 +5,15 @@ const balanceSchema = new mongoose.Schema({}, { strict: false });
 const userSchema = new mongoose.Schema({
   publicAddress: { type: String, required: true, unique: true },
   tokens: { type: [String], default: [] },
-  balances: { type: Map, of: Number, default: {} }
-});
+  balances: { type: Map, of: Number, default: {} },
+  dailyBridgeUsage: {
+    type: Map,
+    of: new mongoose.Schema({
+      date: String, // YYYY-MM-DD
+      total: { type: Number, default: 0 }
+    }),
+    default: {}
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
