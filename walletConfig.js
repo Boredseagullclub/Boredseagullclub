55cd40917ccc46d2727a564b51a2320178b733fc
Bored Seagull Club <134111642+Boredseagullclub@users.noreply.github.com>
2026-03-01 11:22:05 -0800

    Create walletConfig.js

---
+++ walletConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  HOT_WALLETS: {
+    XRP: process.env.XRP_HOT_WALLET,
+    XDC: process.env.XDC_HOT_WALLET,
+    FLR: process.env.FLR_HOT_WALLET,
+    XLM: process.env.XLM_HOT_WALLET,
+    HBAR: process.env.HBAR_HOT_WALLET,
+    ALGO: process.env.ALGO_HOT_WALLET,
+  }
+};
