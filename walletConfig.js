6ae9d5ce94c050b709a3460c7775ae7952891be1
Bored Seagull Club <134111642+Boredseagullclub@users.noreply.github.com>
2026-03-19 19:54:25 -0700

    Update walletConfig.js

--- walletConfig.js
+++ walletConfig.js
@@ -6,5 +6,10 @@ module.exports = {
     XLM: process.env.XLM_HOT_WALLET,
     HBAR: process.env.HBAR_HOT_WALLET,
     ALGO: process.env.ALGO_HOT_WALLET,
+  },
+  // 🔥 Add your limits here
+  DAILY_LIMITS: {
+    SeagullCash: "25000000",
+    SeagullCoin: "100000",
   }
 };
