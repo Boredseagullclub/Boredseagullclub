1c8d9ad22cc631f92f98ab8abb215ce9628cb5bb
Bored Seagull Club <134111642+Boredseagullclub@users.noreply.github.com>
2026-03-19 19:52:52 -0700

    Update depositQueue.js

--- services/queues/depositQueue.js
+++ services/queues/depositQueue.js
@@ -1,8 +1,1 @@
-const { Queue } = require('bullmq');
-const IORedis = require('ioredis');
 
-const connection = new IORedis(process.env.REDIS_URL);
-
-const depositQueue = new Queue('deposit-queue', { connection });
-
-module.exports = depositQueue;
