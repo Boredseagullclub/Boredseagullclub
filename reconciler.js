53d552016d869752cc9ed75e28f83b4915b6cc7b
Bored Seagull Club <134111642+Boredseagullclub@users.noreply.github.com>
2026-03-24 20:46:35 -0700

    Update reconciler.js

--- reconciler.js
+++ reconciler.js
@@ -8,6 +8,8 @@ const Decimal = require('decimal.js');
 const axios = require('axios');
 const logger = require('../utils/logger');
 const config = require('../config');
+const { updateLastAuditResult } = require('../middleware/solvencyGuard');
+
 
 async function performFullAudit() {
   logger.info({ module: 'Reconciler', event: 'audit_started' });
