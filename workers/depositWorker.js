49d3ed61715a0a8cf1775404ef36c91c98d808e4
Bored Seagull Club <134111642+Boredseagullclub@users.noreply.github.com>
2026-03-28 18:27:21 -0700

    Update depositWorker.js

--- workers/depositWorker.js
+++ workers/depositWorker.js
@@ -1,31 +1,55 @@
-const { Worker } = require('bullmq');
-const IORedis = require('ioredis');
 const mongoose = require('mongoose');
 const logger = require('../utils/logger');
 const Deposit = require('../models/Deposit');
-const User = require('../models/User');
 const { processDepositById } = require('../services/DepositProcessor');
 
-const connection = new IORedis(process.env.REDIS_URL);
+async function startDepositWorker() {
+  logger.info({ module: 'DepositWorker', event: 'start', mode: 'ChangeStream' });
 
-const worker = new Worker(
-  'deposit-queue',
-  async job => {
-    const { depositId } = job.data;
-    await processDepositById(depositId);
-  },
-  {
-    connection,
-    concurrency: 10
-  }
-);
+  // 1. Create a "watch" on the Deposits collection
+  // We only care about 'insert' operations where status is 'DETECTED'
+  const depositStream = Deposit.watch([
+    {
+      $match: {
+        operationType: 'insert',
+        'fullDocument.status': 'DETECTED'
+      }
+    }
+  ], { fullDocument: 'updateLookup' });
 
-worker.on('failed', (job, err) => {
-  logger.error({
-    module: 'DepositWorker',
-    depositId: job?.data?.depositId,
-    error: err.message
+  depositStream.on('change', async (change) => {
+    const depositId = change.fullDocument._id;
+    
+    try {
+      logger.debug({ module: 'DepositWorker', event: 'job_received', depositId });
+      
+      // Pass the ID to your existing processor
+      await processDepositById(depositId);
+      
+    } catch (err) {
+      logger.error({
+        module: 'DepositWorker',
+        event: 'processing_failed',
+        depositId,
+        error: err.message
+      });
+    }
   });
-});
 
-module.exports = worker;
+  depositStream.on('error', (err) => {
+    logger.fatal({ module: 'DepositWorker', event: 'stream_error', error: err.message });
+    // Graceful restart logic
+    setTimeout(startDepositWorker, 5000);
+  });
+}
+
+// Self-healing: Also run a "Catch-Up" scan on boot 
+// in case the worker was down while deposits came in.
+async function catchUp() {
+  const missed = await Deposit.find({ status: 'DETECTED' }).select('_id');
+  for (const dep of missed) {
+    await processDepositById(dep._id).catch(() => {});
+  }
+}
+
+module.exports = { startDepositWorker, catchUp };
